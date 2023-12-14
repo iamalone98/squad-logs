423c5e6fef607e1d5370eee5aa9ac06676e4ad4f
ACTEPUKC <makakakakala@gmail.com>
2023-12-14 14:25:05 +0200

    new parsers

--- src/logsReader/parsers/index.ts
+++ src/logsReader/parsers/index.ts
@@ -1,8 +1,41 @@
 import EventEmitter from 'events';
 import { playerConnected } from './playerConnected';
 import { playerDisconnected } from './playerDisconnected';
+import { adminBroadcast } from './adminBroadcast';
+import { deployableDamaged } from './deployableDamaged';
+import { newGame } from './newGame';
+import { playerDamaged } from './playerDamaged';
+import { playerDied } from './playerDied';
+import { playerPossess } from './playerPossess';
+import { playerRevived } from './playerRevived';
+import { playerSuicide } from './playerSuicide';
+import { playerUnpossess } from './playerUnpossess';
+import { playerWonded } from './playerWounded';
+import { roundEnded } from './roundEnded';
+import { roundTickets } from './roundTickets';
+import { roundWinner } from './roundWinner';
+import { squadCreated } from './squadCreated';
+import { vehicleDamaged } from './vehicleDamaged';
 
-const parsers = [playerConnected, playerDisconnected];
+const parsers = [
+  playerConnected,
+  playerDisconnected,
+  adminBroadcast,
+  deployableDamaged,
+  newGame,
+  playerDamaged,
+  playerDied,
+  playerPossess,
+  playerRevived,
+  playerSuicide,
+  playerUnpossess,
+  playerWonded,
+  roundEnded,
+  roundTickets, 
+  roundWinner,
+  squadCreated,
+  vehicleDamaged
+];
 
 export const parseLine = (line: string, emitter: EventEmitter) => {
   parsers.forEach((f) => {
