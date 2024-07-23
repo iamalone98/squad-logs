de23932b459efc7400b37cd395f08a30f035455c
ACTEPUKC <makakakakala@gmail.com>
2024-07-23 21:52:25 +0300

    Add parser for ApplyExplosiveDamage log line and event

---
+++ src/logsReader/parsers/applyExplosiveDamage.ts
@@ -0,0 +1,23 @@
+import { LogsReaderEvents } from '../../events';
+import { TApplyExplosiveDamage } from '../../types';
+
+export const applyExplosiveDamage = (line: string) => {
+  const matches = line.match(
+    /^\[([0-9.:-]+)]\[([ 0-9]*)]LogSquadTrace: \[DedicatedServer](?:ASQProjectile::)?ApplyExplosiveDamage\(\): HitActor=.+ DamageCauser=(.+) DamageInstigator=BP_PlayerController_C_(.+) ExplosionLocation=V\(X=.+, Y=.+, Z=.+/,
+  );
+
+  if (matches) {
+    const data: TApplyExplosiveDamage = {
+      raw: matches[0],
+      time: matches[1],
+      chainID: matches[2],
+      deployable: matches[3],
+      playerController: matches[4],
+      event: LogsReaderEvents.EXPLOSIVE_DAMAGED,
+    };
+
+    return data;
+  }
+
+  return null;
+};
