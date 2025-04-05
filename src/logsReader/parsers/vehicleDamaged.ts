869f922af9a277afd7cb1e51151a0b6fa5dba5ee
lACTEPUKCl <113736434+lACTEPUKCl@users.noreply.github.com>
2025-04-06 00:47:54 +0300

    fix: vehicle damaged (#9)

--- src/logsReader/parsers/vehicleDamaged.ts
+++ src/logsReader/parsers/vehicleDamaged.ts
@@ -3,7 +3,7 @@ import { TVehicleDamaged } from '../../types';
 
 export const vehicleDamaged = (line: string) => {
   const matches = line.match(
-    /^\[([0-9.:-]+)]\[([ 0-9]*)]LogSquadTrace: \[DedicatedServer]ASQVehicleSeat::TraceAndMessageClient\(\): (.+): (.+) damage taken by causer (.+) instigator \(Online Ids: (.+?)\) EOS: ([0-9a-f]{32}) steam: (\d{17}) health remaining (.+)/,
+    /^\[([0-9.:-]+)]\[([ 0-9]*)]LogSquadTrace: \[DedicatedServer](?:ASQVehicleSeat::)?TraceAndMessageClient\(\): (.+): (.+) damage taken by causer (.+) instigator \(Online Ids: (.+?)\) EOS: ([0-9a-f]{32}) steam: (\d{17}) health remaining (.+)/,
   );
 
   if (matches) {
