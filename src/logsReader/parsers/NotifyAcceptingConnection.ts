c483c37459ba7eb77d485d3469421f086b603981
ACTEPUKC <makakakakala@gmail.com>
2025-08-16 14:19:48 +0300

    added a new parser

---
+++ src/logsReader/parsers/NotifyAcceptingConnection.ts
@@ -0,0 +1,22 @@
+import { LogsReaderEvents } from '../../events';
+import { TNotifyAcceptingConnection } from '../../types';
+
+export const notifyAcceptingConnection = (line: string) => {
+  const matches = line.match(
+    /^\[([0-9.:-]+)]\[([ 0-9]*)]LogNet: NotifyAcceptingConnection accepted from: (\d{1,3}(?:\.\d{1,3}){3}):(\d+)$/,
+  );
+
+  if (matches) {
+    const data: TNotifyAcceptingConnection = {
+      raw: matches[0],
+      time: matches[1],
+      chainID: matches[2],
+      ip: matches[3],
+      event: LogsReaderEvents.PLAYER_ACCEPTING_CONNECTION,
+    };
+
+    return data;
+  }
+
+  return null;
+};
